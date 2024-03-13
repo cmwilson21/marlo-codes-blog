e86cefcd8ea13e40a5ecf69ae49f09fb0a8a64c3
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2024-03-13 18:31:36 -0500

    nav bar items added

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,7 +1,20 @@
 import Link from "next/link";
+import profileImg from "@/public/coffee-and-code.jpg";
+import Image from "next/image";
 
 const Logo = () => {
-  return <Link>Logo</Link>;
+  return (
+    <Link href="/" className="flex items-center text-dark">
+      <div className="w-20 overflow-hidden mr-2">
+        <Image
+          src={profileImg}
+          alt="Profile Picture - temporary image"
+          className="w-full h-auto rounded-full"
+        />
+      </div>
+      <span className="font-bold text-xl">MarloCodes</span>
+    </Link>
+  );
 };
 
 export default Logo;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,32 @@
+import { LinkedinIcon } from "@/project files/Icons";
+import Logo from "./Logo";
+import Link from "next/link";
+import { GithubIcon } from "./Icons";
+
 const Header = () => {
   return (
     <header className="w-full p-4 px-10 flex items-center justify-between">
-      index
+      <Logo />
+      <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mr-2">
+          About
+        </Link>
+        <Link href="/contact" className="mr-2">
+          Contact
+        </Link>
+        <button>T</button>
+      </nav>
+      <div>
+        <a href="http://example.com" className="inline-block w-6 h-6 mr-4">
+          <LinkedinIcon />
+        </a>
+        <a href="http://example.com" className="inline-block w-6 h-6 mr-4">
+          <GithubIcon />
+        </a>
+      </div>
     </header>
   );
 };
